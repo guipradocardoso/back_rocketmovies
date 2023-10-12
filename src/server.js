6cae72415fb67980c25205cda3b4344b1626b19e
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-10-12 12:01:48 -0300

    add controller, and users routes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 
+const routes = require("./routes")
+
 const app = express();
 
-app.get("/", (request, response) => {
-   response.send("Hello, world!")
-})
+app.use(express.json());
+
+app.use(routes);
+
+app.post("/users", (request, response) => {
+   const { name, email, password } = request.body;
+
+   response.json({ name, email, password });
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
